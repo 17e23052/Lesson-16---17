eaa44d5e1af13be03d922c8fdd0e2e4232b83a27
Mathman <17e23052@marling.gloucs.sch.uk>
2020-11-02 11:39:37 +0000

    Password

---
+++ main.py
@@ -0,0 +1,10 @@
+stored_pass = "password"
+password = ""
+pass_mismatch = stored_pass != password
+
+while pass_mismatch:
+  print("Enter your password:")
+  password = input()
+  pass_mismatch = stored_pass != password
+
+print("Access granted")
