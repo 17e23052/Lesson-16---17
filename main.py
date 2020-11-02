271a66219647b26b0a24db215d76599ec9e3774c
Mathman <17e23052@marling.gloucs.sch.uk>
2020-11-02 11:45:13 +0000

    password, max 3 guesses

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from time import sleep
 stored_pass = "password"
 password = ""
+guesses = 0
 pass_mismatch = stored_pass != password
 
 while pass_mismatch:
+  guesses = guesses + 1
+  if guesses > 3:
+    print("Maximum guesses reached. Locked out for 10 minutes")
+    sleep(600)
   print("Enter your password:")
   password = input()
   pass_mismatch = stored_pass != password
