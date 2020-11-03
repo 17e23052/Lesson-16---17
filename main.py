98d95f223e0d71b585e396f8073f7257d42abea1
Mathman <17e23052@marling.gloucs.sch.uk>
2020-11-03 11:03:31 +0000

    FizzBuzz game UNFINISHED

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-for number in range(1,16):
+#UNFINISHED BROKEN FIZZBUZZ GAME
+number = 1
+if number % 3 != 0 and number % 5 != 0:
+  ans = number
+elif number % 3 == 0 and number % 5 != 0:
+  ans = "Fizz"
+elif number % 3 != 0 and number % 5 == 0:
+  ans = "Buzz"
+elif number % 3 == 0 and number % 5 == 0:
+  ans = "FizzBuzz"
 
-  if number <= 15 and number % 3 != 0 and number % 5 != 0:
-    print(number)
-  if number % 3 == 0 and number % 5 != 0:
-    print("Fizz")
-  if number % 3 != 0 and number % 5 == 0:
-    print("Buzz")
-  if number % 3 == 0 and number % 5 == 0:
-    print("FizzBuzz")
+print("Welcome to FizzBuzz! Enter a number, Fizz, Buzz or FizzBuzz.")
+guess = input()
+number = number + 1
+if guess == ans:
+  print("Next:")
+elif guess != ans:
+  print("Incorrect! Game over.")
+elif number == 15:
+  print("You completed Fizzbuzz! Thanks for playing.")
